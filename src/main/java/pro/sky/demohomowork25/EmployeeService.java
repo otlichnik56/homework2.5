2208d0f2c9900fcda3badb5ab194bf3e4cc5bc4b
Hyp <otlichnik56@yandex.ru>
2022-09-15 21:30:07 +0500

    не доделан

--- src/main/java/pro/sky/demohomowork25/EmployeeService.java
+++ src/main/java/pro/sky/demohomowork25/EmployeeService.java
@@ -8,22 +8,7 @@ import java.util.stream.Collectors;
 
 @Service
 public class EmployeeService implements EmployeeServiceImp {
-    private  final Map<String, Employee> employers = new HashMap<>(Map.of(
-            "123",
-            new Employee("Иванов", "Иван", 50000, 3),
-            "456",
-            new Employee("Иванов1", "Иван1", 500200, 3),
-            "789",
-            new Employee("Иванов2", "Иван2", 501000, 1),
-            "147",
-            new Employee("Иванов3", "Иван3", 507000, 2),
-            "258",
-            new Employee("Иванов4", "Иван4", 550000, 2),
-            "369",
-            new Employee("Иванов5", "Иван5", 530000, 3),
-            "159",
-            new Employee("Иванов6", "Иван6", 503000, 3)
-    ));
+    private  final Map<String, Employee> employers = new HashMap<>(Map.of());
 
     @Override
     public Map<String, Employee> getEmployers(){
